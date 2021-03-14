d25e2838f30778bd2667bdeaf4a12390405c9f45
Pandeyn-OSU <80611385+Pandeyn-OSU@users.noreply.github.com>
2021-03-13 20:27:00 -0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,17 +1,11 @@
 """
-Unit testing the calculator app
+Calculator app with basic methods.
 """
 
-import calculator
+
+def add(a, b):
+    return a + b
 
 
-class TestCalculator:
-
-    def test_add(self):
-        assert 5 == calculator.add(1, 4)
-
-    def test_subtract(self):
-        assert 2 == calculator.subtract(5, 3)
-
-    def test_multiply(self):
-        assert 2 == calculator.multiply(1, 2)
+def subtract(a, b):
+    return a - b
